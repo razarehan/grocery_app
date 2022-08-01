da03696fcbaa54d363ec9dda0b0a3b34816e716e
Rehan Raza <rhn.raza121@gmail.com>
2022-08-01 12:22:38 +0530

    Adding recipe.model and recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Cucumber Sandwich',`2 ounces cream cheese, at room temperature
+                                    1 tablespoon low-fat plain Greek yogurt
+                                    1 tablespoon sliced fresh chives
+                                    1 tablespoon chopped fresh dill`, 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg'),
+    new Recipe('Caitlin Bensel', `1 cup all-purpose flour
+                    2 eggs
+                    ½ cup milk
+                    ½ cup water
+                    ¼ teaspoon salt`, 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F02%2F22%2F16383-basic-crepes-mfs_003.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
