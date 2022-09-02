7056c17e84bfe378f64f61b2527d89ebd0773907
Rehan Raza <rhn.raza121@gmail.com>
2022-09-02 22:19:56 +0530

    Adding ingredients to the recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe  } from './recipe.model';
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,22 @@ export class RecipeService {
     new Recipe('Cucumber Sandwich',`2 ounces cream cheese, at room temperature
                                     1 tablespoon low-fat plain Greek yogurt
                                     1 tablespoon sliced fresh chives
-                                    1 tablespoon chopped fresh dill`, 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg'),
+                                    1 tablespoon chopped fresh dill`, 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg',
+                                    [
+                                      new Ingredient('Cheese', 2),
+                                      new Ingredient('Yogurt', 1),
+                                      new Ingredient('Chives', 2)
+                                    ]),
     new Recipe('Caitlin Bensel', `1 cup all-purpose flour
                     2 eggs
                     ½ cup milk
                     ½ cup water
-                    ¼ teaspoon salt`, 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F02%2F22%2F16383-basic-crepes-mfs_003.jpg')
+                    ¼ teaspoon salt`, 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F02%2F22%2F16383-basic-crepes-mfs_003.jpg',
+                    [
+                      new Ingredient('Flour', 1),
+                      new Ingredient('Eggs', 2),
+                      new Ingredient('Milk', 1)
+                    ])
   ];
   constructor() { }
 
