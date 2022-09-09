0897ec1269b01ba7903c10bb49578bd4f04cd775
Rehan Raza <rhn.raza121@gmail.com>
2022-09-10 00:20:46 +0530

    Add Ingredent while recipe edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -59,6 +59,14 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
   
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl
+      })
+    );
+  }
   get controls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
