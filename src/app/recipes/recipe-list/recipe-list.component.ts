76b791e2954fdf8919a9e45aa7a971c25acd7954
Rehan Raza <rhn.raza121@gmail.com>
2022-09-10 20:14:42 +0530

    add removeFunctionality on EditRecipe to remove ingredient

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,20 +9,9 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
-  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
+export class RecipeListComponent implements OnInit, OnDestroy {
+  recipesChangeSub!: Subscription;
   recipes: Recipe[] = [];
-  // recipes: Recipe[] = [
-  //   new Recipe('Cucumber Sandwich',`2 ounces cream cheese, at room temperature
-  //                                   1 tablespoon low-fat plain Greek yogurt
-  //                                   1 tablespoon sliced fresh chives
-  //                                   1 tablespoon chopped fresh dill`, 'https://static.onecms.io/wp-content/uploads/sites/44/2022/03/01/cucumber-sandwich.jpg'),
-  //   new Recipe('Caitlin Bensel', `1 cup all-purpose flour
-  //                   2 eggs
-  //                   ½ cup milk
-  //                   ½ cup water
-  //                   ¼ teaspoon salt`, 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F02%2F22%2F16383-basic-crepes-mfs_003.jpg')
-  // ];
   constructor(private recipeService: RecipeService, 
               private route: ActivatedRoute,
               private router: Router) { }
@@ -31,12 +21,13 @@ export class RecipeListComponent implements OnInit {
   }
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-    this.recipeService.recipesChanged
+    this.recipesChangeSub = this.recipeService.recipesChanged
       .subscribe((recipes: Recipe[]) => {
         this.recipes = recipes;
       })
   }
-  // onRecipeSelected(recipe: Recipe) {
-  //   this.recipeWasSelected.emit(recipe);
-  // }
+
+  ngOnDestroy(): void {
+    this.recipesChangeSub.unsubscribe();
+  }
 }
