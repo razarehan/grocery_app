6e85ec0bce2dd88a9ef7a06f568eeedb135382a0
Rehan Raza <rhn.raza121@gmail.com>
2022-09-11 20:39:03 +0530

    Store data in Firebase database

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,9 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  // @Output() featureSelected = new EventEmitter<string>();
-
-  // onSelect(feature: string) {
-  //   this.featureSelected.emit(feature);
-  // }
+  constructor(private dsService: DataStorageService) {
+  }
+  onSaveData() {
+    this.dsService.storeRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient) { }
-  
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://grocery-app-5f077-default-rtdb.firebaseio.com/recipes.json', recipes)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
